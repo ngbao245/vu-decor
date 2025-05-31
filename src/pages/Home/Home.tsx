caca738790b40df913f5014628a81797dbca76e1
BaoBiBo <ng.hoangbao03@gmail.com>
2025-05-31 10:51:28 +0700

    style(Home): change background color of sections to a consistent light gray for improved visual coherence

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,12 +7,12 @@ import FAQ from "../../components/FAQ/FAQ"
 
 export const Home = () => {
     return (
-        <div className="min-h-screen flex flex-col w-full">
+        <div className="min-h-screen flex flex-col w-full bg-[#f5f5f3]">
             <div>
                 {/* <div className="h-16 bg-[#2f2f2f]"></div> */}
                 <Hero />
             </div>
-            <div className="flex bg-white min-h-[700px] px-4 py-8 md:px-8 lg:px-20 items-center">
+            <div className="flex bg-[#f5f5f3] min-h-[700px] px-4 py-8 md:px-8 lg:px-20 items-center">
                 <ServiceHome />
             </div>
             <div className="flex bg-white min-h-[700px] px-4 py-8 md:px-8 lg:px-20 items-center"
@@ -20,7 +20,7 @@ export const Home = () => {
             >
                 <WhyChooseUs />
             </div>
-            <div className="flex bg-white min-h-[800px] px-4 py-8 md:px-8 lg:px-20 items-center">
+            <div className="flex bg-[#f5f5f3] min-h-[800px] px-4 py-8 md:px-8 lg:px-20 items-center">
                 <GalleryHome />
             </div>
 
@@ -31,7 +31,7 @@ export const Home = () => {
                 </div>
                 <ProjectCate />
             </div> */}
-            <div className="bg-white">
+            <div className="bg-[#f5f5f3]">
                 <StoryTeam />
             </div>
             <FAQ />
